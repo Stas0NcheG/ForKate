9f9eb69d77a30cda52e1ddd40252e84d25259b40
Stas0NcheG <stas_098@mail.ru>
2020-02-26 15:26:04 +0300

    new

--- FirstGame/src/main/java/FirstGame/Matrix.java
+++ FirstGame/src/main/java/FirstGame/Matrix.java
@@ -5,18 +5,22 @@ import java.util.Scanner;
 public class Matrix {
     public static void main(String[] args) {
         Random ran = new Random();
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите размер массива: ");
-        int w = in.nextInt();
-        int q = in.nextInt();
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Введите размер массива: ");
+//        int w = in.nextInt();
+//        int q = in.nextInt();
+        int q = (int)(1+(Math.random()*(10)));
+        int w = (int)(1+(Math.random()*(q)));
+        System.out.println(w);
+        System.out.println(q);
         int[][] a = new int[w][q];
 
 
         for (int i = 0; i < w; i++) {
             for (int j = 0; j < q; j++) {
                 a[i][j] = ran.nextInt(9);
-                System.out.print(a[i][j]);
-                System.out.println(" ");
+//                System.out.print(a[i][j]);
+//                System.out.println(" ");
             }
         }
             int s = 0;
