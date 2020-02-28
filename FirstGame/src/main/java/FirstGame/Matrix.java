31c7a8fe5d7fc0ebb1320d188ca57111cf7b0af9
Stas0NcheG <stas_098@mail.ru>
2020-02-28 11:58:48 +0300

    new

--- FirstGame/src/main/java/FirstGame/Matrix.java
+++ FirstGame/src/main/java/FirstGame/Matrix.java
@@ -9,8 +9,8 @@ public class Matrix {
 //        System.out.print("Введите размер массива: ");
 //        int w = in.nextInt();
 //        int q = in.nextInt();
-        int q = (int)(1+(Math.random()*(10)));
-        int w = (int)(1+(Math.random()*(q)));
+        int q = (int) (1 + (Math.random() * (10)));
+        int w = (int) (1 + (Math.random() * (q)));
         System.out.println(w);
         System.out.println(q);
         int[][] a = new int[w][q];
@@ -23,17 +23,23 @@ public class Matrix {
 //                System.out.println(" ");
             }
         }
-            int s = 0;
-            int b = 0;
-            int c = 0;
-            int n = a.length;
-            for (int f = 0; f < n; ++f) {
-                b += a[f][f];
-                s += a[f][n - 1 - f];
-            }
+        int s = 0;
+        int b = 0;
+        int c = 0;
+        int n = a.length;
+        for (int f = 0; f < n; ++f) {
+            b += a[f][f];
+            s += a[f][n - 1 - f];
+        }
             c = b + s;
-            System.out.println(s);
             System.out.println(b);
+            System.out.println(s);
             System.out.println(c);
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                System.out.print(" " + a[i][j] + " ");
+            }
+            System.out.println();
         }
     }
+}
