73256ddb892c5502d4fe8f193dd61cc3a0269744
Stas0NcheG <stas_098@mail.ru>
2020-02-28 12:05:33 +0300

    new

--- FirstGame/src/main/java/FirstGame/Matrix.java
+++ FirstGame/src/main/java/FirstGame/Matrix.java
@@ -11,16 +11,14 @@ public class Matrix {
 //        int q = in.nextInt();
         int q = (int) (1 + (Math.random() * (10)));
         int w = (int) (1 + (Math.random() * (q)));
-        System.out.println(w);
-        System.out.println(q);
+        System.out.println(w + "-Высота");
+        System.out.println(q + "-Ширина");
         int[][] a = new int[w][q];
 
 
         for (int i = 0; i < w; i++) {
             for (int j = 0; j < q; j++) {
                 a[i][j] = ran.nextInt(9);
-//                System.out.print(a[i][j]);
-//                System.out.println(" ");
             }
         }
         int s = 0;
@@ -32,9 +30,9 @@ public class Matrix {
             s += a[f][n - 1 - f];
         }
             c = b + s;
-            System.out.println(b);
-            System.out.println(s);
-            System.out.println(c);
+            System.out.println(b + "-Сумма основной диагонали");
+            System.out.println(s + "-Сумма побочной диагонали");
+            System.out.println(c + "-Общая сумма");
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[0].length; j++) {
                 System.out.print(" " + a[i][j] + " ");
